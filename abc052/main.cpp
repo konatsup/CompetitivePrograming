b97fa1a87fb8e4e13f4d6a40f5b608e9cb6b3f2b
Konatsup <konatsukomatsu@gmail.com>
2018-01-23 20:09:14 +0900

    Add abc052_a

--- abc052/main.cpp
+++ abc052/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int a,b,c,d;
+    std::cin >>a>>b>>c>>d;
+    std::cout << (a*b>c*d? a*b:c*d) << std::endl;
     return 0;
 }
