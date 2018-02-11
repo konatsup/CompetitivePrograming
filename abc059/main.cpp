6f6c38ced35a28b9c5af0754c67db9da924f124f
Konatsup <konatsukomatsu@gmail.com>
2018-02-11 21:36:44 +0900

    #Add abc059__a

---
+++ abc059/main.cpp
@@ -0,0 +1,3 @@
+#include <iostream>
+using namespace std;
+int main() { char s1[200], s2[200], s3[200];cin >> s1 >> s2 >> s3;cout << (char)(s1[0]-32) << (char)(s2[0]-32)<<(char)(s3[0]-32)<<endl;return 0; }
