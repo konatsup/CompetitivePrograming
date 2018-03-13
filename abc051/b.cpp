477f6afa18d6d98d1664cc14551b7302e2b9dc4e
Konatsup <konatsukomatsu@gmail.com>
2018-03-14 07:14:07 +0900

    #Add abc051_b

--- abc051/b.cpp
+++ abc051/b.cpp
@@ -3,8 +3,14 @@
 using namespace std;
 
 int main() {
-    int k, s;
+    int k, s, count = 0;
+    int x, y;
     cin >> k >> s;
-
+    for (x = 0; x < k+1; x++) {
+        for (y = 0; y < k+1; y++) {
+            if (s-x-y >=0 && s-x-y <=k) count++;
+        }
+    }
+    cout << count << endl;
     return 0;
 }
