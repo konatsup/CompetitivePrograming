11d3dc7f30052b7afc641ea6affe8b050a6632d4
Konatsup <konatsukomatsu@gmail.com>
2018-03-14 08:43:49 +0000

    #Fixed short

--- abc088/main.cpp
+++ abc088/main.cpp
@@ -1,8 +1,3 @@
 #include <iostream>
-
-int main() {
-    int n,a;
-    std::cin >> n>>a;
-    std::cout << ((n%500 <= a)?"Yes":"No") << std::endl;
-    return 0;
-}
+int n,a;
+int main() {    std::cin >> n>>a; std::cout << ((n%500 <= a)?"Yes":"No") << std::endl;}
