c39ab02400a453dbc2c5e00233a655dd0cd6af8c
Konatsup <konatsukomatsu@gmail.com>
2018-03-14 09:29:11 +0900

    #Add abc090_a

--- abc090/main.cpp
+++ abc090/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    char a[5], b[5], c[5];
+    std::cin >> a >> b >> c;
+    std::cout << a[0] << b[1] << c[2] << std::endl;
     return 0;
 }
