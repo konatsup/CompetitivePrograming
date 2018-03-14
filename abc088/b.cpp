402d605e87750c17a3bb40e956b7d822a76c4058
Konatsup <konatsukomatsu@gmail.com>
2018-03-14 17:16:50 +0900

    #Add abc088_b

---
+++ abc088/b.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n;
+    int a = 0;
+    int b = 0;
+    int array[110];
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> array[i];
+    }
+    sort(array, array + n, greater<int>());
+
+    for (int i = 0; i < n; i++) {
+        if (i % 2 == 0) {
+            a += array[i];
+        } else {
+            b += array[i];
+        }
+    }
+    cout << a - b << endl;
+
+    return 0;
+}
