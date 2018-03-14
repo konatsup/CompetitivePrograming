1bb9e32495b3e489b6fa776b2bf5190a17e9497c
Konatsup <konatsukomatsu@gmail.com>
2018-03-14 09:28:44 +0000

    #Add abc067_a

--- abc067/main.cpp
+++ abc067/main.cpp
@@ -1,7 +1,8 @@
 #include <bits/stdc++.h>
 
 int main(){
-    
-    
+    int a, b;
+    std::cin >> a >> b;
+    std::cout << ((a%3 ==0 || b%3==0 || (a+b)%3 == 0)?"Possible":"Impossible") <<std::endl;
     return 0;
 }
