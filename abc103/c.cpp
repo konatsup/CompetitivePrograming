21c848c0c36d68f3709375a7e4c46719bbd97912
Konatsup <konatsukomatsu@gmail.com>
2018-07-21 14:52:00 +0000

    #Add abc103c

--- abc103/c.cpp
+++ abc103/c.cpp
@@ -2,13 +2,15 @@
 using namespace std;
 
 int main(){
-    int n;
+    int n,sum=0;
     int a[10010];
     cin>> n;
     for(int i=0; i< n; i++){
         cin >> a[i];
     }
-    
-    
+    for(int i=0; i< n; i++){
+        sum += a[i]-1;
+    }
+    cout << sum <<endl;
     return 0;
 }
